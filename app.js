c5cd86fd35d04000a32075f01728b08efcf7a6fb
rasullyusufov <rasullyusufov@gmail.com>
2022-01-20 15:53:43 +0500

    Bugs fixed Added functions added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+const select = document.getElementById('select')
 const form = document.getElementById('form')
 const titleEl = document.querySelector('.title');
 const inputEl = document.getElementById('input')
@@ -7,36 +7,54 @@ const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const button = document.getElementById('button')
 
-let word ;
-let typed ;
+let word;
+let typed;
 let score = 0
+let time = 10;
+let level = 'Eyse'
 
-if(navigator.onLine == true){
-    function reload(){
-    fetch(`https://random-word-api.herokuapp.com/word`).then(function(datas){
-        return datas.json()
-    }).then(getdata)
-    function getdata(data){
-        word = data
-        titleEl.textContent = word;
-        document.getElementById('input').placeholder = word;
-       
+if (navigator.onLine == true) {
+    function reload() {
+        fetch(`https://random-word-api.herokuapp.com/word`).then(function (datas) {
+            return datas.json()
+        }).then(getdata)
+        function getdata(data) {
+            word = data
+            titleEl.textContent = word;
+            document.getElementById('input').placeholder = word;
+        }
+        scoreEl.textContent = `Score ${score}`
+    } reload()
+    form.addEventListener('input', (e) => {
+        typed = inputEl.value
+        e.preventDefault()
+        if (typed.toLowerCase() == word) {
+            reload()
+            inputEl.value = ''
+            score++
+            scoreEl.textContent = `Score ${score}`
+            if (level == 'Eyse') {
+                time += 5
+            }
+            else if (level == 'Medium') {
+                time += 4
+            }
+            else if (level == 'Hard') {
+                time += 3
+            }
+        }
+    })
+    select.addEventListener('change', () => {
+        level = select.value
+    })
+    const setGameOver = setInterval(timeFUnction, 1000);
+function timeFUnction() {
+    time--
+    timeEl.textContent = `Time: ${time}`
+    if (time <= 0) {
+        clearInterval(setGameOver)
     }
-    scoreEl.textContent = `Score ${score}`
-}reload()
-form.addEventListener('input' , (e)=>{
-    typed = inputEl.value.toLowerCase()
-    e.preventDefault()
-  if(typed == word){
-   reload()
-   inputEl.value = ''
-   score++
-   scoreEl.textContent = `Score ${score}`
-  }
-})
-
-}else if(navigator.onLine == false){
-    scoreEl.textContent = `You are not connected to the internet ... or the connection is not good!`
-    console.log(22);
 }
-
+} else if (navigator.onLine == false) {
+    scoreEl.textContent = `You are not connected to the internet ... or the connection is not good!`
+}
